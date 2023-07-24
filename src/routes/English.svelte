32125c179db91b7e5f9cf6b19d009cc3d34a14e6
Sajjad Taghinezhad <sajjad@taghinezhad.me>
2023-07-24 19:02:44 +0330

    feat(english): create element for english progress page

--- src/routes/English.svelte
+++ src/routes/English.svelte
@@ -1,13 +1,20 @@
-<!-- About.svelte -->
-<script>
-  // Your script logic here
-</script>
-
-<main>
-  <h1>English Page</h1>
-  <!-- Your content here -->
-</main>
-
-<style>
-  /* Your styles here */
-</style>
+<div>
+	<div class="grid place-content-center place-items-center gap-2" data-theme="dark">
+		<div class="card lg:card-side bg-base-100 shadow-xl mt-10 w-100">
+			<div class="grid p-5">
+				<progress class="progress progress-info w-56 h-5" value="10" max="100" />
+				<progress class="progress progress-info w-56 h-5" value="10" max="100" />
+				<progress class="progress progress-info w-56 h-5" value="40" max="100" />
+				<progress class="progress progress-info w-56 h-5" value="70" max="100" />
+				<progress class="progress progress-info w-56 h-5" value="100" max="100" />
+			</div>
+			<div class="card-body">
+				<h2 class="card-title">English Learning Progress</h2>
+				<p>Click the button to listen on Spotiwhy app.</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Listen</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
