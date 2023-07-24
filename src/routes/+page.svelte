6e979c18238bf8a3772da9bfd84ed3ba2bc8d5c8
Sajjad Taghinezhad <sajjad@taghinezhad.me>
2023-07-24 19:02:04 +0330

    feat(page): update page file code , remove unused codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
-  <button class="btn">Hello daisyUI</button>
+<script>
+	import English from "./English.svelte";
+</script>
+
+<English/>
