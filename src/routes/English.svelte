2914269465e0c9496fd9b51c1d4363ca92a0fa70
Sajjad Taghinezhad <sajjad@taghinezhad.me>
2023-07-24 17:34:30 +0330

    init project

---
+++ src/routes/English.svelte
@@ -0,0 +1,13 @@
+<!-- About.svelte -->
+<script>
+  // Your script logic here
+</script>
+
+<main>
+  <h1>English Page</h1>
+  <!-- Your content here -->
+</main>
+
+<style>
+  /* Your styles here */
+</style>
